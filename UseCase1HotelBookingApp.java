76ceb82ceae78cb319659e83d13daae975758bc7
SURJITH JAYAKUMAR <jksurjith@gmail.com>
2026-04-01 08:50:25 +0530

    Fix formatting issues in UseCase1HotelBookingApp

--- UseCase1HotelBookingApp.java
+++ UseCase1HotelBookingApp.java
@@ -1,11 +1,10 @@
 public class UseCase1HotelBookingApp {
 
     public static void main(String[] args) {
-
-        System.out.println("======================================");
+        System.out.println
         System.out.println(" Welcome to Book My Stay Application ");
         System.out.println(" Hotel Booking System v1.0 ");
-        System.out.println("======================================");
+        System.out.println
         System.out.println("Your journey to comfortable stays begins here!");
         System.out.println("Application started successfully.");
     }
